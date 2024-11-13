f9c1881cc8d18ede353cb2c99aea68c3fa8926fc
ambdhict <adhict.222@gmail.com>
2024-11-13 22:42:43 +0800

    added home component to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './service/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'home', component: DashboardComponent, canActivate: [AuthGuard]},
+
+    {path : '**', component: DashboardComponent}
 ];
