4d96fd7c6816a6a119ef26619b23952d05f49310
ambdhict <adhict.222@gmail.com>
2024-11-13 20:31:40 +0800

    added app setting model

---
+++ src/app/model/app.setting.ts
@@ -0,0 +1,8 @@
+export class AppSetting{
+    public urlApi: string = 'http://localhost:4200/';
+    public tname: string = '7E6F59EE-90A3-416B-BF1F-1C36A12B98DB';
+    public app: string = 'WebApp';
+    public fromApp: boolean = true;
+    public tokenName: string = 'WebToken';
+    public errorMessage: string = 'Web Application cannot connect to API';
+}
