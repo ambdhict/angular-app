4141daa98f461d4a7ee46bcad77559f2d7ab8a9c
ambdhict <adhict.222@gmail.com>
2024-11-13 22:42:54 +0800

    added auth guard

--- src/app/service/auth.guard.ts
+++ src/app/service/auth.guard.ts
@@ -1,5 +1,30 @@
-import { CanActivateFn } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { CanActivate, Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { AppSetting } from '../model/app.setting';
+import { AuthenticationService } from './authentication.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+Injectable({providedIn: 'root'})
+
+export class AuthGuard implements CanActivate {
+  appSet: AppSetting = new AppSetting(); 
+  router = inject(Router); 
+  authServive = inject(AuthenticationService); 
+  
+  canActivate(router: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean { 
+    var haveaccess = true; 
+    var user = this.authServive.currentUser; 
+
+    if (user.username != ''){ 
+      var currDate = new Date 
+      
+      if (currDate > user.expirationDate) { 
+        alert('Session expired you will redirect to Login page.'); 
+        localStorage.removeItem(this.appSet.tokenName); 
+        window.location.href = '/login'; } //TODO: check rights 
+      } 
+      else { 
+        window.location.href = '/login'; 
+      } 
+    return haveaccess;
+  }
 };
